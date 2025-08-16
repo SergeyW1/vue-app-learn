5ce0c0f03014d650134f00e3e0c568fe694b86a5
Sergey Volkov <f.focus1377@gmail.com>
2025-08-16 12:47:28 +0300

    16 august exercise

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,33 @@
 <script setup>
-  import FancyCard from './components/FancyCard.vue';
+  import { ref } from 'vue';
+  import TodoList from './components/TodoList.vue';
+
+  /**
+   * Задача:
+   *
+   * Создай новый компонент TodoList.vue.
+   *
+   * Передай в него список задач (todos) через пропсы.
+   *
+   * Внутри TodoList.vue отобрази этот список в виде списка с элементами, используя v-for.
+   *
+   * Каждый элемент списка должен показывать текст задачи.
+   */
+  const todos = ref([
+    { id: 100, title: 'Выучить Javascript' },
+    { id: 101, title: 'Выучить Английский' },
+    { id: 102, title: 'Выучить Vue' },
+    { id: 103, title: 'Выучить все на свете' },
+    { id: 104, title: 'Завести' },
+  ]);
+
+  const handleDeleteTodo = (id) => {
+    todos.value = todos.value.filter((item) => item.id !== id);
+  };
 </script>
 
 <template>
-  <FancyCard title="Task Info" border-color="green">
-    <p>This task is high priority and must be done today.</p>
-
-    <template #footer>
-      <button>Mark as done</button>
-    </template>
-  </FancyCard>
-
-  <FancyCard title="Note" border-color="orange">
-    <p>Remember to call the client tomorrow morning.</p>
-  </FancyCard>
+  <TodoList :todos="todos" @delete="handleDeleteTodo">
+    <h2>Список</h2>
+  </TodoList>
 </template>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  defineProps({
+    todos: Array,
+  });
+
+  const emit = defineEmits(['delete']);
+
+  const handleDeleteTodo = (id) => {
+    emit('delete', id);
+    console.log(id);
+  };
+</script>
+
+<template>
+  <slot></slot>
+
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <span>{{ todo.title }}</span>
+
+      <button @click="handleDeleteTodo(todo.id)">Удалить</button>
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
